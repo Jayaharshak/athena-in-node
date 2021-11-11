5f8acfbb8b121ffe6d8a9dc1ea84be0a9233aab7
Jay kandimalla <jkandimalla@dmdconnects.com>
2021-11-12 00:53:08 +0530

    indentation fix

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ const AthenaQuery = require('./athenaQuery');
 const async = require('async');
 
 async.waterfall([
-        (callback)=>{
-            AthenaQuery.createQueryExecutionId(callback);
-        },
-        async (query, callback)=>{
-            async.retry({
-                times: 60,
-                interval: 10000
-            }, AthenaQuery.checkQueryCreateStatus.bind(query), (err, result) => {
-                console.log("CHECKING QUERY STATUS", err, result);
-                if (!err) {
-                   callback(null, query)
-                }
-                else{
-                    callback(err)
-                }
-            });
-        },
-        (query, callback)=>{
-            
-            AthenaQuery.getQueryResultByExecutionId(query.QueryExecutionId, (err, result)=>{
-                console.log("QUERY RESULTS BY EXEC.ID", err, result);
-                callback(null, result, query)
-            })
-        },
-        (queryResult, query, callback)=>{
-            AthenaQuery.stopQueryExecutionId(query.QueryExecutionId, (err, result)=>{
-                console.log("%%%%STOP CALL%%%%");
-                callback(err, queryResult)
-            });
-        }
-        ],(error, result) => {
-        	console.log("******result*******",error, result);
+    (callback) => {
+        AthenaQuery.createQueryExecutionId(callback);
+    },
+    (query, callback) => {
+        async.retry({
+            times: 60,
+            interval: 10000
+        }, AthenaQuery.checkQueryCreateStatus.bind(query), (err, result) => {
+            console.log("CHECKING QUERY STATUS", err, result);
+            if (!err) {
+                callback(null, query)
+            }
+            else {
+                callback(err)
+            }
+        });
+    },
+    (query, callback) => {
+
+        AthenaQuery.getQueryResultByExecutionId(query.QueryExecutionId, (err, result) => {
+            console.log("QUERY RESULTS BY EXEC.ID", err, result);
+            callback(null, result, query)
         })
+    },
+    (queryResult, query, callback) => {
+        AthenaQuery.stopQueryExecutionId(query.QueryExecutionId, (err, result) => {
+            console.log("%%%%STOP CALL%%%%");
+            callback(err, queryResult)
+        });
+    }
+], (error, result) => {
+    console.log("******result*******", error, result);
+})
